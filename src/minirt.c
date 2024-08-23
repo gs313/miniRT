521823283dcad1683e4b5dfa9cb9008fc1e19977
Prvdnx <129741476+Prvdnx@users.noreply.github.com>
2024-08-23 15:26:29 +0700

    update minirt.c

--- src/minirt.c
+++ src/minirt.c
@@ -3,22 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   minirt.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: scharuka <scharuka@42.fr>                  +#+  +:+       +#+        */
+/*   By: ookamonu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/16 01:53:42 by scharuka          #+#    #+#             */
-/*   Updated: 2024/08/20 21:18:42 by scharuka         ###   ########.fr       */
+/*   Updated: 2024/08/23 15:25:34 by ookamonu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minirt.h"
 
-int	main()
+void	hook(void *param)	// for key press events (ESC and Q key)
+{
+	mlx_t	*mlx;
+	mlx = param;
+	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE) || mlx_is_key_down(mlx, MLX_KEY_Q))
+	{
+		mlx_close_window(mlx);
+	}
+}
+
+int	main(void)
 {
 	// void *mlx = mlx_init();
 	// void *win = mlx_new_window(mlx, 640, 360, "Tutorial Window");
 	void	*mlx = mlx_init(WIN_WIDTH, WIN_HEIGHT, "Tutorial Window", true);
 	mlx_image_t	*img = mlx_new_image(mlx, 640, 360);
 	mlx_image_to_window(mlx, img, 0, 0);
+	mlx_loop_hook(mlx, hook, mlx); 	// hook function for key press events
 	mlx_loop(mlx);
+	mlx_terminate(mlx);
 	return (0);
 }
