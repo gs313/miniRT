810a0a3a6ac25b63338f3244dffeffd26d1bd63e
Prvdnx <129741476+Prvdnx@users.noreply.github.com>
2024-08-23 15:58:24 +0700

    update

--- miniRT Test/src/sphere.c
+++
@@ -1,9 +0,0 @@
-#include "../inc/minirt.h"
-
-void sphere_init(t_sphere *sphere, double x, double y, double z, double radius, uint8_t r, uint8_t g, uint8_t b) {
-    sphere->coord = (t_vector){x, y, z};
-    sphere->radius = radius;
-    sphere->color[0] = r;
-    sphere->color[1] = g;
-    sphere->color[2] = b;
-}
