b8bd346bed47c8937a513e9229e62269ac44b3d5
gs313 <sirapat1charu@gmail.com>
2024-08-23 17:13:10 +0700

    add files to make file

--- Makefile
+++ Makefile
@@ -6,14 +6,14 @@
 #    By: scharuka <scharuka@42.fr>                  +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/08/16 01:48:06 by scharuka          #+#    #+#              #
-#    Updated: 2024/08/20 21:29:29 by scharuka         ###   ########.fr        #
+#    Updated: 2024/08/23 17:12:00 by scharuka         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME = MiniRT
 
 SRC_DIR = src/
-FILES = minirt.c
+FILES = minirt.c sphere.c camera.c
 SRC = $(addprefix $(SRC_DIR), $(FILES))
 
 CC = cc
--- src/camera.c
+++ src/camera.c
@@ -6,7 +6,7 @@
 /*   By: scharuka <scharuka@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 12:34:25 by scharuka          #+#    #+#             */
-/*   Updated: 2024/08/21 11:51:18 by scharuka         ###   ########.fr       */
+/*   Updated: 2024/08/23 17:12:22 by scharuka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,6 @@ void	viewport_init(t_scene *scene)
 	t_vector	u = vec_norm(vec_cross(scene->cam.dir, vec_init(0, 1, 0)));
 	t_vector	v = vec_norm(vec_cross(u, scene->cam.dir));
 	double		aspect_ratio = (double)WIN_WIDTH / (double)WIN_HEIGHT;
-	double		focal_length = 1.0;
 	double		viewport_width = 2.0f * tan((scene->cam.dec * M_PI / 180) / 2);
 	double		viewport_height = viewport_width / aspect_ratio;
 	scene->view.delta_u = vec_scale(u, (viewport_width/(double)WIN_WIDTH));
@@ -35,6 +34,6 @@ void	viewport_init(t_scene *scene)
 	scene->view.pixel00_loc = vec_sub(scene->view.pixel00_loc, vec_scale(u, viewport_width/2));
 	scene->view.pixel00_loc = vec_sub(scene->view.pixel00_loc, vec_scale(v, viewport_height/2));
 	scene->view.pixel00_loc = vec_add(scene->view.pixel00_loc, vec_scale(scene->view.delta_u, 0.5));
-	scene->view.pixel00_loc = vec_add(scene->view.pixel00_loc, vec_scale(scene->view.delta_v, 0.5));	
+	scene->view.pixel00_loc = vec_add(scene->view.pixel00_loc, vec_scale(scene->view.delta_v, 0.5));
 	//still need to test
 }
