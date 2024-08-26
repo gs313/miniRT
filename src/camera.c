76e37e5ff18d0d01e2ff5523917a4aa9a024d0e2
gs313 <sirapat1charu@gmail.com>
2024-08-27 01:23:13 +0700

    norminette camera.c

--- src/camera.c
+++ src/camera.c
@@ -6,7 +6,7 @@
 /*   By: scharuka <scharuka@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 12:34:25 by scharuka          #+#    #+#             */
-/*   Updated: 2024/08/27 00:50:16 by scharuka         ###   ########.fr       */
+/*   Updated: 2024/08/27 01:22:39 by scharuka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 t_camera	camera_init(t_vector coord, t_vector dir, unsigned int dec)
 {
 	t_camera	cam;
+
 	cam.coord = coord;
 	cam.dir = dir;
 	cam.dec = dec;
@@ -24,23 +25,28 @@ t_camera	camera_init(t_vector coord, t_vector dir, unsigned int dec)
 void	viewport_init(t_scene *scene)
 {
 	t_viewport	view;
-	t_vector	u = vec_norm(vec_cross(scene->cam.dir, vec_init(0, 1, 0)));
-	t_vector	v = vec_norm(vec_cross(u, scene->cam.dir));
-	double		aspect_ratio = (double)WIN_WIDTH / (double)WIN_HEIGHT;
-	double		viewport_width = 2.0f * tan((scene->cam.dec * M_PI / 180) / 2);
-	double		viewport_height = viewport_width / aspect_ratio;
+	t_vector	u;
+	t_vector	v;
+	double		viewport_width;
+	double		viewport_height;
 
-	view.delta_u = vec_scale(u, (viewport_width/(double)WIN_WIDTH));
-	view.delta_v = vec_scale(v, (viewport_height/(double)WIN_HEIGHT));
-	view.pixel00_loc = vec_add(scene->cam.coord , vec_norm(scene->cam.dir));
-	view.pixel00_loc = vec_sub(view.pixel00_loc, vec_scale(u, viewport_width/2));
-	view.pixel00_loc = vec_sub(view.pixel00_loc, vec_scale(v, viewport_height/2));
+	u = vec_norm(vec_cross(scene->cam.dir, vec_init(0, 1, 0)));
+	v = vec_norm(vec_cross(u, scene->cam.dir));
+	viewport_width = 2.0f * tan((scene->cam.dec * M_PI / 180) / 2);
+	viewport_height = viewport_width / ((double)WIN_WIDTH / (double)WIN_HEIGHT);
+	view.delta_u = vec_scale(u, (viewport_width / (double)WIN_WIDTH));
+	view.delta_v = vec_scale(v, (viewport_height / (double)WIN_HEIGHT));
+	view.pixel00_loc = vec_add(scene->cam.coord, vec_norm(scene->cam.dir));
+	view.pixel00_loc = vec_sub(view.pixel00_loc,
+			vec_scale(u, viewport_width / 2));
+	view.pixel00_loc = vec_sub(view.pixel00_loc,
+			vec_scale(v, viewport_height / 2));
 	view.pixel00_loc = vec_add(view.pixel00_loc, vec_scale(view.delta_u, 0.5));
 	view.pixel00_loc = vec_add(view.pixel00_loc, vec_scale(view.delta_v, 0.5));
 	scene->view = view;
 }
 
-int		render (t_scene *scene)
+int	render(t_scene *scene)
 {
 	t_vector	pixel_loc;
 	t_vector	ray_dir;
@@ -55,11 +61,11 @@ int		render (t_scene *scene)
 		x = 0;
 		while (x < WIN_WIDTH)
 		{
-			pixel_loc = vec_add(scene->view.pixel00_loc, vec_scale(scene->view.delta_u, x));
+			pixel_loc = vec_add(scene->view.pixel00_loc,
+					vec_scale(scene->view.delta_u, x));
 			pixel_loc = vec_add(pixel_loc, vec_scale(scene->view.delta_v, y));
 			ray_dir = vec_norm(vec_sub(pixel_loc, scene->cam.coord));
 			color = trace_ray(scene, scene->cam.coord, ray_dir);
-			// printf ("pixel_loc: %f %f %f\n", pixel_loc.x, pixel_loc.y, pixel_loc.z);
 			mlx_put_pixel(scene->img, x, y, color);
 			x++;
 		}
@@ -71,20 +77,19 @@ int		render (t_scene *scene)
 uint32_t	background_color(t_vector dir)
 {
 	double	t;
+
 	t = 0.5 * (dir.y + 1.0);
 	return (rgb_to_int((1.0 - t) * 255, (1.0 - t) * 255, 255));
 }
 
 uint32_t	trace_ray(t_scene *scene, t_vector origin, t_vector dir)
 {
-	t_hit	hit;
-	uint32_t color;
+	t_hit		hit;
+	uint32_t	color;
 
 	hit = hit_closest(scene, origin, dir);
 	if (hit.obj_id == -1)
 		return (background_color(dir));
 	color = cal_color(hit, scene);
-	// color = rgb_to_int(scene->obj[hit.obj_id].r, scene->obj[hit.obj_id].g, scene->obj[hit.obj_id].b);
-	// printf("color: %u\n", color);
 	return (color);
 }
