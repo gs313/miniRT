807c1018e326f4d3e9b57673d62d2e0060751c2d
Prvdnx <129741476+Prvdnx@users.noreply.github.com>
2024-09-03 21:42:08 +0700

    update is_cam_in_obj.c

--- src/parser/is_cam_in_obj.c
+++ src/parser/is_cam_in_obj.c
@@ -6,7 +6,7 @@
 /*   By: ookamonu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 02:39:25 by ookamonu          #+#    #+#             */
-/*   Updated: 2024/09/03 12:08:07 by ookamonu         ###   ########.fr       */
+/*   Updated: 2024/09/03 21:40:58 by ookamonu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,13 @@
 
 bool	is_camera_inside_sphere(t_camera cam, t_object obj)
 {
-	double distance = sqrt(pow(cam.coord.x - obj.coord.x, 2) +
-						   pow(cam.coord.y - obj.coord.y, 2) +
-						   pow(cam.coord.z - obj.coord.z, 2));
-    printf("Debug: Sphere - Camera distance: %f, Sphere radius: %f\n", distance, obj.d / 2);
+	double	distance;
+
+	distance = sqrt(pow(cam.coord.x - obj.coord.x, 2)
+			+ pow(cam.coord.y - obj.coord.y, 2)
+			+ pow(cam.coord.z - obj.coord.z, 2));
+	printf("Debug: Sphere - Camera distance: %f, Sphere radius: %f\n",
+		distance, obj.d / 2);
 	if (distance < obj.d / 2)
 	{
 		printf("Error: Camera is inside a sphere\n");
@@ -37,7 +40,6 @@ bool	is_camera_inside_cylinder(t_camera cam, t_object obj)
 	projection = vec_dot(cam_to_axis, obj.dir);
 	closest_point = vec_add(obj.coord, vec_scale(obj.dir, projection));
 	distance = vec_len(vec_sub(cam.coord, closest_point));
-    printf("Debug: Cylinder - Camera distance: %f, Cylinder radius: %f, Projection: %f\n", distance, obj.d / 2, projection);
 	if (distance < obj.d / 2 && projection >= 0 && projection <= obj.h)
 	{
 		printf("Error: Camera is inside a cylinder\n");
@@ -53,8 +55,7 @@ bool	is_camera_inside_plane(t_camera cam, t_object obj)
 
 	cam_to_plane = vec_sub(cam.coord, obj.coord);
 	distance = vec_dot(cam_to_plane, obj.dir);
-    printf("Debug: Plane - Camera distance: %f, Plane height: %f\n", distance, obj.h);
-    if (fabs(distance) < 1e-6) // Check if the camera is exactly on the plane
+	if (fabs(distance) < 1e-6)
 	{
 		printf("Error: Camera is inside a plane\n");
 		exit(EXIT_FAILURE);
@@ -62,7 +63,8 @@ bool	is_camera_inside_plane(t_camera cam, t_object obj)
 	return (false);
 }
 
-void	is_camera_inside_object(t_camera cam, t_object *obj, unsigned int obj_count)
+void	is_camera_inside_object(t_camera cam, t_object *obj,
+		unsigned int obj_count)
 {
 	unsigned int	i;
 
@@ -84,52 +86,3 @@ void	is_camera_inside_object(t_camera cam, t_object *obj, unsigned int obj_count
 		i++;
 	}
 }
-
-// bool	is_camera_inside_object(t_camera cam, t_object *obj, unsigned int obj_count)
-// {
-// 	unsigned int	i;
-// 	i = 0;
-// 	while (i < obj_count)
-// 	{
-// 		if (obj[i].type == SPHERE) 
-// 		{
-// 			double distance = sqrt(pow(cam.coord.x - obj[i].coord.x, 2) +
-// 								pow(cam.coord.y - obj[i].coord.y, 2) +
-// 								pow(cam.coord.z - obj[i].coord.z, 2));
-// 			if (distance < obj[i].d / 2)
-// 			{
-// 				printf("Error: Camera is inside a sphere\n");
-// 				exit(EXIT_FAILURE);
-// 			}
-// 		}
-// 		else if (obj[i].type == CYLINDER)
-// 		{
-// 			t_vector	cam_to_axis;
-// 			double		projection;
-// 			t_vector	closest_point;
-// 			cam_to_axis = vec_sub(cam.coord, obj[i].coord);
-// 			projection = vec_dot(cam_to_axis, obj[i].dir);
-// 			closest_point = vec_add(obj[i].coord, vec_scale(obj[i].dir, projection));
-// 			double distance = vec_len(vec_sub(cam.coord, closest_point));
-// 			if (distance < obj[i].d / 2 && projection >= 0 && projection <= obj[i].h)
-// 			{
-// 				printf("Error: Camera is inside a cylinder\n");
-// 				exit(EXIT_FAILURE);
-// 			}
-// 		}
-// 		else if (obj[i].type == PLANE)
-// 		{
-// 			t_vector	cam_to_plane;
-// 			double		distance;
-// 			cam_to_plane = vec_sub(cam.coord, obj[i].coord);
-// 			distance = vec_dot(cam_to_plane, obj[i].dir);
-// 			if (distance > 0 && distance < obj[i].h)
-// 			{
-// 				printf("Error: Camera is inside a plane\n");
-// 				exit(EXIT_FAILURE);
-// 			}
-// 		}
-// 		i++;
-// 	}
-// 	return (false);
-// }
